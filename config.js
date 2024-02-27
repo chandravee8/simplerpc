cbe529e094a6a14c9e73c8dbe639d851dc1e2bf3
chandravee8 <161228452+chandravee8@users.noreply.github.com>
2024-02-27 10:52:19 +0000

    Pending changes exported from your codespace

--- config.js
+++ config.js
@@ -4,18 +4,18 @@ const
 function reloadPresence(client) {
     const activity = new discord.RichPresence()
       //more command https://discordjs-self-v13.netlify.app/#/docs/docs/main/class/RichPresence or https://github.com/aiko-chan-ai/discord.js-selfbot-v13/blob/main/Document/RichPresence.md
-        .setApplicationId("1119170929747050506") //You can replace with your own bot application id
+        .setApplicationId("1187589793337581611") //You can replace with your own bot application id
         .setType("PLAYING") //PLAYING, STREAMING, LISTENING
-        .setName("MIAW") //name of your activity
-        .setDetails("miawww...") //detail activity
+        .setName("CHANDU ON TOP") //name of your activity
+        .setDetails("NO REPLY = I AM AFK") //detail activity
         .setStartTimestamp(global.startTime)
         .setAssetsLargeImage("mp:icons/1120755328267583559/a65ec7583c9986ea1faf8c3f02259916.webp")
         .setAssetsLargeText("Miawww")
         .setAssetsSmallImage("mp:avatars/390832676778803200/f72c077c48fb9b5c42a1d44330ec5f22.png")
-        .setAssetsSmallText("Zaxerion")
-        .addButton('Facebook', "https://www.facebook.com/zaxerionn")
-        .addButton('Paypal', "https://paypal.me/zaxerion")
+        .setAssetsSmallText("Chandu")
+        .addButton('Discord', "https://discord.com/invite/FGU7vhnmPK")
+        .addButton('Join', "https://discord.com/invite/FGU7vhnmPK")
     client.user.setActivity(activity.toJSON())
-    client.user.setStatus("idle")
+    client.user.setStatus("dnd")
 };
 module.exports = reloadPresence;
