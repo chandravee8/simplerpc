698672103e6d29b4532210c32e67cd95505884eb
chandravee8 <161228452+chandravee8@users.noreply.github.com>
2024-08-30 07:28:59 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,12 +9,12 @@ function reloadPresence(client) {
         .setName("CHANDU ON TOP") //name of your activity
         .setDetails("NO REPLY = OFFLINE") //detail activity
         .setStartTimestamp(global.startTime)
-        .setAssetsLargeImage("https://cdn.discordapp.com/avatars/1054360093526855680/a_ba60e397ab487d6b554a12466ba3311a.gif")
+        .setAssetsLargeImage("https://cdn.discordapp.com/attachments/1104302917386764388/1278896413937500160/a_e4025f82b15ff91e521829d2a27e86b0.gif?ex=66d27899&is=66d12719&hm=0fb8fc286f249673eedb69ffdb72359c59573efec98582745afcb95873d265b2&")
         .setAssetsLargeText("Chandu")
         .setAssetsSmallImage("https://cdn.discordapp.com/attachments/1210929928862695457/1211999933398392843/image0.jpg?ex=65f03de5&is=65ddc8e5&hm=b113809debc6732fee38b1fbd7351610b158fa745c2e5194c1ac8dbfe6586cc7&")
         .setAssetsSmallText("Chandu")
         .addButton('Discord', "https://discord.com/invite/FGU7vhnmPK")
-        .addButton('Join', "https://discord.com/invite/FGU7vhnmPK")
+        .addButton('Join', "https://discord.com/invite/legalsmp")
     client.user.setActivity(activity.toJSON())
     client.user.setStatus("dnd")
 };
